7af5c663f0064e9e566a3a5c15b2c72bd62b50e2
陈佳 <9076569+on1314@user.noreply.gitee.com>
2021-05-21 16:16:45 +0800

    项目初始化准备

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+//引入初始化样式文件
+import './assets/css/reset200802.css'
+
+//引入 fastclick.js 解决onclick的300延迟问题 npm install fastclick -save
+import FastClick from 'fastclick'
+FastClick.attach(document.body);
+
+//引入 图标css
+import './assets/css/iconfont.css'
+
 new Vue({
   router,
   store,
